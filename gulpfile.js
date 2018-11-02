18b8fa1e9492caa1734f8749fbf945def76f9fbe
Antonio Jesús <antoniojesusvazquezmunozz@gmail.com>
2018-11-02 12:58:29 +0100

    build(workspace): add files

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const minify = require('gulp-minify');
+const connect = require('gulp-connect');
+
+const AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10',
+];
+
+gulp.task('html', () => {
+  gulp
+    .src(['./index.html'])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+      }),
+    )
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', () => {
+  gulp
+    .src('./src/**/*.css')
+    .pipe(
+      autoprefixer({
+        browsers: AUTOPREFIXER_BROWSERS,
+      }),
+    )
+    .pipe(csso())
+    .pipe(gulp.dest('./dist/src'))
+    .pipe(connect.reload());
+});
+
+gulp.task('jsmain', () => {
+  gulp
+    .src('./app.js')
+    .pipe(minify())
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', () => {
+  gulp
+    .src('./src/**/*.js')
+    .pipe(minify())
+    .pipe(gulp.dest('./dist/src'))
+    .pipe(connect.reload());
+});
+
+gulp.task('connect', () => {
+  connect.server({
+    root: './',
+    livereload: true,
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.watch(
+    ['./index.html', './src/**/*.css', './app.js', './src/**/*.js'],
+    ['html', 'css', 'jsmain', 'js'],
+  );
+});
+
+gulp.task('default', ['watch', 'connect']);
